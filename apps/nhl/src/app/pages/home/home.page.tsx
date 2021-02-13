150f33cf11843bb8b6a750e0b8a0dc9630ae4089
Matija077 <matija.prs@gmail.com>
2021-02-13 19:20:36 +0100

    fetch, navbar, meta tag

--- apps/nhl/src/app/pages/home/home.page.tsx
+++ apps/nhl/src/app/pages/home/home.page.tsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { AppBar} from '@material-ui/core';
 
 function HomePage() {
+
+    useEffect(() => {
+        fetch("http://localhost:3333/api/teams").then(function resolved(result) {
+            return result.json();
+        })
+        .then(function data(data: any) {
+            console.log(data);
+        })
+        .catch(function rejected(error: any) {
+            console.log(error);
+        })
+    }, []);
+
     return (
-        <div>"Home"</div>
+        <>
+        <AppBar
+            position="sticky"
+        >
+            NHL TEAMS
+        </AppBar>
+        </>
     );
 }
 
