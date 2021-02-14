3dafb06a55a2fd087f5c25d604060e4ed4231ed1
Matija077 <matija.prs@gmail.com>
2021-02-14 11:46:50 +0100

    add anchors to each team

--- apps/nhl/src/app/components/team-row/teamRow.component.tsx
+++ apps/nhl/src/app/components/team-row/teamRow.component.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { RowStyles, ListItemTextStyles, ListStyles } from './teamRow.component.styles';
+import { Link } from 'react-router-dom';
+import { RowStyles, ListItemTextStyles } from './teamRow.component.styles';
 import { TeamType } from '../../utility/types';
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -14,9 +15,11 @@ function TeamRow({ location, id, name, venue }: TeamType) {
     const classes = useStyles();
 
     return (
-        <RowStyles button>
-                <ListItemTextStyles style={{backgroundColor: color}} primary={name} secondary={location + "/" + venue} classes={{root: classes.root}} ></ListItemTextStyles>
-        </RowStyles>
+        <Link to={`/team/${id}`}>
+            <RowStyles button>
+                    <ListItemTextStyles style={{backgroundColor: color}} primary={name} secondary={location + "/" + venue} classes={{root: classes.root}} ></ListItemTextStyles>
+            </RowStyles>
+        </Link>
     );
 }
 
