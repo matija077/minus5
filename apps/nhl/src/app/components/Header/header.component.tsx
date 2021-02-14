7dab77820047ad5649589112ac9a0cf5e6b8a9e2
Matija077 <matija.prs@gmail.com>
2021-02-14 17:51:25 +0100

    update header

--- apps/nhl/src/app/components/Header/header.component.tsx
+++ apps/nhl/src/app/components/Header/header.component.tsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { withRouter } from 'react-router-dom';
 
 import { AppBar} from '@material-ui/core';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Switch from '@material-ui/core/Switch';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import { withStyles } from '@material-ui/core/styles';
+
+const ToggleDarkModeSwitch = withStyles({
+    switchBase: {
+        '&$checked + $track': {
+            backgroundColor: "#ffffff",
+            opacity: 1
+        }
+    },
+    checked: {},
+    track: {},
+  })(Switch);
+
 
 function Header(props) {
     //const history = useHistory();
     console.log(props);
+    const [darkMode, setDarkMode] = useState<boolean>(false);
+    const toggleDarkMode = () => setDarkMode((state) => !state);
+
     return (
         <AppBar
             color={"secondary"}
@@ -20,7 +38,11 @@ function Header(props) {
                 <Typography variant={"h5"}>
                     NHL TEAMS
                 </Typography>
-
+                <FormControlLabel
+                control={<ToggleDarkModeSwitch checked={darkMode} onChange={toggleDarkMode} aria-label="dark mode switch" color="primary"/>}
+                label={darkMode ? 'Dark' : 'Normal'}
+                text
+            />
             </Toolbar>
         </AppBar>
     );
