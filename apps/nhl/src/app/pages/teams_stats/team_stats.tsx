2f85c54c3902f2789d6b23b27af1737dc7f9fd1c
Matija077 <matija.prs@gmail.com>
2021-02-15 17:50:03 +0100

    sem idecent working

--- apps/nhl/src/app/pages/teams_stats/team_stats.tsx
+++ apps/nhl/src/app/pages/teams_stats/team_stats.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams, useLocation } from 'react-router-dom';
 import Spinner from '../../components/spinner/spinner.component';
 import { useFetch } from '../../utility/customHooks';
@@ -11,6 +11,12 @@ import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import ListItem, { ListItemProps } from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import List from '@material-ui/core/List';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import Collapse from '@material-ui/core/Collapse';
 
 const url = "http:///localhost:3333/api/teams";
 
@@ -26,28 +32,40 @@ const useStyles = makeStyles((theme: Theme) =>
   }),
 );
 
+enum dataTypes {
+    "rankings" = "rankings",
+    "stats" = "stats"
+}
+
 function TeamStats(props: any) {
-    console.log(props);
+    // hooks
     const { id } = useParams();
     const { state: passedState } = useLocation();
+    const [openRankings, setOpenRankings] = useState<boolean>(false);
+    const [openStats, setOpenStats] = useState<boolean>(true);
     const classes = useStyles();
 
+    // data fetching
     const urlWithParams = url + "/" + id;
     const [teamDetails, loading] = useFetch<TeamDetailsType>(urlWithParams);
-    console.log(loading);
 
+    // handlers
+    function handleClickExpand(event: any) {
+        const type = event.currentTarget.dataset.type;
+
+        if (type === dataTypes.stats) {
+            setOpenStats(stats => !stats);
+        } else if (type === dataTypes.rankings) {
+            setOpenRankings(rankings => !rankings);
+        }
+    }
+
+    // component related data
     const venueCity = passedState.venue + "/" + passedState.location;
-    const tabletLandscape = useMediaQuery('(min-width: 1023px) and (orientation: landscape)');
-    const tablePortrait = useMediaQuery('(min-width: 600px) and (orientation: portrait)');
-    const tablet = tablePortrait || tabletLandscape;
-    console.log("portrait " + tablePortrait)
-    /*const mobileLandscape = useMediaQuery('(min-width: 1023px) and (orientation: landscape)');
-    const mobilePortrait = useMediaQuery('(min-width: 600px) and (orientation: portrait)');
-    const mobile = mobilePortrait || mobileLandscape;
-    console.log(tablet);*/
-    const mobile = false;
 
-    //(min-width: 600px) and (orientation: "portrait"), (min-width: 1000px) and (orientation: "landscape")
+    const mobileLandscape = useMediaQuery('(max-width: 1023px) and (orientation: landscape)');
+    const mobilePortrait = useMediaQuery('(max-width: 600px) and (orientation: portrait)');
+    const mobile = mobilePortrait || mobileLandscape;
 
     return (
         <Grid container spacing={2} className={classes.grid}>
@@ -61,7 +79,22 @@ function TeamStats(props: any) {
                !loading
                     ? mobile
                         ? <Grid container spacing={2}>
-                            "mobile"
+                            <List>
+                                <ListItem button onClick={handleClickExpand} data-type={dataTypes.stats}>
+                                    <ListItemText primary="Stats" />
+                                    {openStats ? <ExpandLess /> : <ExpandMore />}
+                                </ListItem>
+                                <Collapse in={openStats} timeout="auto">
+                                    <TeamDetailsItem data={teamDetails.stats} />
+                                </Collapse>
+                                <ListItem button onClick={handleClickExpand} data-type={dataTypes.rankings}>
+                                    <ListItemText primary="Rankings" />
+                                    {openRankings ? <ExpandLess /> : <ExpandMore />}
+                                </ListItem>
+                                <Collapse in={openRankings} timeout="auto">
+                                    <TeamDetailsItem data={teamDetails.ranks} />
+                                </Collapse>
+                        </List>
                         </Grid>
                         : <Grid container spacing={2}>
                             <Grid item xs={6}>
